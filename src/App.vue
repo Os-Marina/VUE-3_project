af0114dcdc555d09529c4c6e86aae483d81cb1ca
Осипчук Марина Владимировна <mosipchuk@phoenixit.ru>
2024-07-03 16:31:11 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <input v-model="person.name">
+   <span>{{ person.name }} -> {{ normalazeName}}</span>
+</template>
+
+<script setup>
+import { ref, computed, reactive } from "vue";
+
+const person = reactive({name: 'Denis'})
+const normalazeName = computed(() => person.name.toUpperCase())
+/*  
+// const count = ref(0)
+// const computedCount = ref(0)
+
+// const computedCount = computed (() => {
+//   if (count.value % 2){
+//     return title.value.toUpperCase()
+//   } return title.value.toLowerCase()
+//   // return count.value++
+// })
+
+// watchEffect(() => {
+//   computedCount.value = count.value + 1
+// })
+
+// watch (() => count.value, (num) => {
+//   computedCount.value = num + 1
+// }) 
+*/
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
