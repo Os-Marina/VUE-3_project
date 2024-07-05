53351605db1e190001e02a0ff92e4914e22250d4
Os-Marina <mr.vinogradskaya@gmail.com>
2024-07-05 05:32:10 +0300

    next

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
+
 <template>
-  <input v-model="person.name">
-   <span>{{ person.name }} -> {{ normalazeName}}</span>
+  <!-- <input :value="task" @input="onInput"> -->
+    <!-- <input v-model="person.name"> -->
+   <!-- <span>{{ person.name }} -> {{ normalazeName}}</span> -->
+  <input v-model="taskTitle">
+  <button @click="addTask">Add task</button>
+  <div class="task-list">
+    <template v-for="task in tasks">
+      <div 
+        v-if="showOnlyCompleted ? task.complete : false"
+        class="task"
+        :class="[{'task--complete' : task.complete}]"
+      >
+        {{ task.title }}
+        <button @click="task.complete = !task.complete">
+          complete
+        </button>
+      </div>
+    </template>
+  </div>
+
+  <!-- <div v-for="value, key in {name: 'Den', wallet: 'sdfsdfsf'}">
+    {{ key }} " {{ value }}"
+  </div> -->
+  <div>
+    <input v-model="showOnlyCompleted" type="checkbox" >
+    Show only completed
+  </div>
+
 </template>
 
-<script setup>
-import { ref, computed, reactive } from "vue";
+<script setup lang="ts">
+import { ref, computed } from "vue";
 
-const person = reactive({name: 'Denis'})
-const normalazeName = computed(() => person.name.toUpperCase())
+type Task = {title: string, complete: boolean}
+const taskTitle = ref('')
+const showOnlyCompleted = ref('false')
+const tasks = ref([] as Task[])
+
+
+function addTask(){
+  tasks.value.push({title: taskTitle.value, complete: true})
+  taskTitle.value = ''
+}
+// function complete(task: ref<Task>){
+
+// }
+/* 
+// function onInput(event: Event){
+//   task.value = (event.target as HTMLInputElement).value
+// }
+*/
+// const normalazeName = computed(() => person.name.toUpperCase())
 /*  
 // const count = ref(0)
 // const computedCount = ref(0)
@@ -27,11 +71,14 @@ const normalazeName = computed(() => person.name.toUpperCase())
 //   computedCount.value = num + 1
 // }) 
 */
-
-
-
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.task-list{
+  display: flex;
+  flex-direction: column;
+}
+.task--complete{
+  text-decoration: line-through;
+}
 </style>
